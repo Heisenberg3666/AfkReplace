a2c8d6bae3400b60a4947523d86b0bc734db94e7
Dylan Morris <dylanmorris330@gmail.com>
2022-04-24 21:15:30 +0100

    Player now inherits the full inventory (including ammo) and also effects.

--- AfkReplace/EventHandler.cs
+++ AfkReplace/EventHandler.cs
@@ -1,8 +1,11 @@
 ﻿using AfkReplace.API;
 using AfkReplace.Exceptions;
+using CustomPlayerEffects;
+using Exiled.API.Enums;
 using Exiled.API.Features;
 using Exiled.Events.EventArgs;
 using MEC;
+using System.Linq;
 
 namespace AfkReplace
 {
@@ -23,15 +26,27 @@ namespace AfkReplace
                 Log.Debug("Random spectator selected.", Plugin.Instance.Config.DebugMode);
                 Timing.CallDelayed(5f, () =>
                 {
+                    // Setting player role, location and health.
                     player.SetRole(ev.Target.Role.Type);
                     Log.Debug($"Spectator ({player.Nickname}) has been set to {player.Role}.", Plugin.Instance.Config.DebugMode);
                     player.Teleport(ev.Target.Position);
                     Log.Debug($"Spectator ({player.Nickname}) has been teleported to {player.Position}.", Plugin.Instance.Config.DebugMode);
-                    player.ClearInventory();
-                    player.AddItem(ev.Target.Items);
-                    Log.Debug($"Spectator ({player.Nickname}) has been given {ev.Target.Nickname}'s items.", Plugin.Instance.Config.DebugMode);
                     player.Health = ev.Target.Health;
                     Log.Debug($"Spectator ({player.Nickname}) has been given {ev.Target.Nickname}'s health.", Plugin.Instance.Config.DebugMode);
+
+                    // Giving player Items and Ammo.
+                    player.ClearInventory();
+                    player.AddItem(ev.Target.Items);
+                    player.AddAmmo(AmmoType.Ammo44Cal, ev.Target.Ammo[ItemType.Ammo44cal]);
+                    player.AddAmmo(AmmoType.Ammo12Gauge, ev.Target.Ammo[ItemType.Ammo12gauge]);
+                    player.AddAmmo(AmmoType.Nato556, ev.Target.Ammo[ItemType.Ammo556x45]);
+                    player.AddAmmo(AmmoType.Nato762, ev.Target.Ammo[ItemType.Ammo762x39]);
+                    player.AddAmmo(AmmoType.Nato9, ev.Target.Ammo[ItemType.Ammo9x19]);
+                    Log.Debug($"Spectator ({player.Nickname}) has been given {ev.Target.Nickname}'s items.", Plugin.Instance.Config.DebugMode);
+                    
+                    // Giving player effects
+                    foreach (PlayerEffect effect  in ev.Target.ActiveEffects)
+                        player.EnableEffect(effect, effect.Duration, true);
                 });
             }
             catch (NotEnoughSpectatorsException)
